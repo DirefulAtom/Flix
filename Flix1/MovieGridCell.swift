1aebf6f388ab5b56844f62310f8d768f66e4d76b
Michael LeClaire <mlanderson@vassar.edu>
2022-02-16 00:26:15 -0500

    Made the layout for the "Superhero" tab bar view look more pleasing to the eye, and called for higher resolution images from the API.

---
+++ Flix1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix1
+//
+//  Created by Michael LeClaire on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
